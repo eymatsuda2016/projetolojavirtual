e4647f0d7109ad11966ec977a99b0ba6057fe072
Eduardo Y Matsuda <eymatsuda2016@outlook.com>
2023-02-25 19:30:05 -0300

    Criação do Projeto

---
+++ src/main/java/com/eduardomatsuda/lojavirtual/ProjetolojavirtualApplication.java
@@ -0,0 +1,12 @@
+package com.eduardomatsuda.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolojavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolojavirtualApplication.class, args);
+	}
+}
