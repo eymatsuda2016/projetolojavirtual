e3a7f6d6c2f9fe5b729d02e68488c11454af4b03
Eduardo Y Matsuda <eymatsuda2016@outlook.com>
2023-02-28 20:01:13 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/eduardomatsuda/lojavirtual/ProjetolojavirtualApplication.java
+++ src/main/java/com/eduardomatsuda/lojavirtual/ProjetolojavirtualApplication.java
@@ -9,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.eduardomatsuda.lojavirtual.domain.Categoria;
+import com.eduardomatsuda.lojavirtual.domain.Produto;
 import com.eduardomatsuda.lojavirtual.repositories.CategoriaRepository;
+import com.eduardomatsuda.lojavirtual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetolojavirtualApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetolojavirtualApplication.class, args);
@@ -27,8 +32,19 @@ public class ProjetolojavirtualApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Peças e Acessorios");
 		Categoria cat2 = new Categoria(null, "Pneus");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1  = new Produto (null, "Auto Radio Original Polo TSI", 2500.00 );
+		Produto p2 = new Produto (null, "Pneu 205 55 16", 550.00);
+		Produto p3  = new Produto(null, "Lanterna Traseira Esquerda Polo", 259.99 );
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
 
