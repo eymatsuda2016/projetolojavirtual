aa8f246cf303e21befe122dbdefdf27e306d4777
Eduardo Y Matsuda <eymatsuda2016@outlook.com>
2023-02-27 21:25:08 -0300

    Criado Repository e Sevice para Categoria

---
+++ src/main/java/com/eduardomatsuda/lojavirtual/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.eduardomatsuda.lojavirtual.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardomatsuda.lojavirtual.domain.Categoria;
+import com.eduardomatsuda.lojavirtual.repositories.CategoriaRepository;
+
+
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
