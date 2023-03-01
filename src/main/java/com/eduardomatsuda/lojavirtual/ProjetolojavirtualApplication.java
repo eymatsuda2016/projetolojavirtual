b05dbfcdd0e7de09a83ffb5d16b28bcb290549ed
Eduardo Y Matsuda <eymatsuda2016@outlook.com>
2023-02-28 23:06:05 -0300

    Criação do Estado e Cidade

--- src/main/java/com/eduardomatsuda/lojavirtual/ProjetolojavirtualApplication.java
+++ src/main/java/com/eduardomatsuda/lojavirtual/ProjetolojavirtualApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.eduardomatsuda.lojavirtual.domain.Categoria;
+import com.eduardomatsuda.lojavirtual.domain.Cidade;
+import com.eduardomatsuda.lojavirtual.domain.Estado;
 import com.eduardomatsuda.lojavirtual.domain.Produto;
 import com.eduardomatsuda.lojavirtual.repositories.CategoriaRepository;
+import com.eduardomatsuda.lojavirtual.repositories.CidadeRepository;
+import com.eduardomatsuda.lojavirtual.repositories.EstadoRepository;
 import com.eduardomatsuda.lojavirtual.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class ProjetolojavirtualApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetolojavirtualApplication.class, args);
@@ -45,6 +55,19 @@ public class ProjetolojavirtualApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Belo Horizonte", est1);
+		Cidade c2 =  new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
 
