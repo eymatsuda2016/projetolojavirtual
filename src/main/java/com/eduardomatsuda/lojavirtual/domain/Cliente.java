5886e0862d7d834f7d80418587897a8cceb53937
Eduardo Y Matsuda <eymatsuda2016@outlook.com>
2023-03-04 18:22:47 -0300

    Construida a Classe Pedido, EstadoPagamanto e Pagamento

--- src/main/java/com/eduardomatsuda/lojavirtual/domain/Cliente.java
+++ src/main/java/com/eduardomatsuda/lojavirtual/domain/Cliente.java
@@ -39,6 +39,10 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set <String> telefones = new HashSet<>();
 	
+	
+	@OneToMany(mappedBy="cliente")	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	private Cliente() {
 	}
 
@@ -86,6 +90,34 @@ public class Cliente implements Serializable{
 	public TipoCliente getTipo() {
 		return TipoCliente.toEnum(tipo);
 	}
+	
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+	
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -103,26 +135,5 @@ public class Cliente implements Serializable{
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	public void setTipo(TipoCliente tipo) {
-		this.tipo = tipo.getCod();
-	}
-
-	public List<Endereco> getEnderecos() {
-		return enderecos;
-	}
-
-	public void setEnderecos(List<Endereco> enderecos) {
-		this.enderecos = enderecos;
-	}
-
-	public Set<String> getTelefones() {
-		return telefones;
-	}
-
-	public void setTelefones(Set<String> telefones) {
-		this.telefones = telefones;
-	}
-	
 	
 }
